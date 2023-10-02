7271e895a197a286df876f5d9fc612ed68a585e8
Eric Fu <eric.fu@mercury.eco>
2023-10-01 18:13:28 -0600

    add logging for req

--- index.js
+++ index.js
@@ -8,31 +8,41 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+function logging(req) {
+  console.info(`${req.method} ${req.originalUrl}, Payload: ${JSON.stringify(req.body)}`);
+}
+
 app.get('/', (req, res) => {
+  logging(req);
   res.send({ Healthy: true });
 });
 
 app.get('/contacts', async (req, res) => {
+  logging(req);
   const contacts = await get();
   res.send(contacts);
 });
 
 app.get('/contacts/:id', async (req, res) => {
+  logging(req);
   const contacts = await getById(req.params.id);
   res.send(contacts);
 });
 
 app.post('/contacts', async (req, res) => {
+  logging(req);
   const result = await post(req.body);
   res.send(result);
 });
 
 app.put('/contacts/:id', async (req, res) => {
+  logging(req);
   const result = await put(req.params.id, req.body);
   res.send(result);
 });
 
 app.delete('/contacts/:id', async (req, res) => {
+  logging(req);
   const result = await deleteById(req.params.id);
   res.send(result);
 });
